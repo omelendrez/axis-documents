65e5e646863f82f6b60adbe4882136d6d03205a1
Omar Melendrez <omar.melendrez@gmail.com>
2023-12-01 22:40:39 -0300

    Test volume

--- test/test.sh
+++ test/test.sh
@@ -3,24 +3,21 @@
 cd /data
 ls
 
-cd /bird-volume
-ls
+mkdir exports
+cd exports
+mkdir certificates
+mkdir id-cards
+mkdir opito
+mkdir welcome-letter
+cd opito
+mkdir csv
+cd /data
+mkdir uploads
+cd uploads
+mkdir foets
+mkdir learner-ids
+mkdir pictures
 
-# mkdir exports
-# cd exports
-# mkdir certificates
-# mkdir id-cards
-# mkdir opito
-# mkdir welcome-letter
-# cd opito
-# mkdir csv
-# cd /data
-# mkdir uploads
-# cd uploads
-# mkdir foets
-# mkdir learner-ids
-# mkdir pictures
+cd /data
 
-# cd /data
-
-# ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
+ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
