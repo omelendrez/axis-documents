4b3c0da8bccffb67f8dae575db6b2f9448aa1573
Omar Melendrez <omar.melendrez@gmail.com>
2023-12-01 22:47:40 -0300

    Get volumne tree

--- test/test.sh
+++ test/test.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-
-cd /data
-ls
-
-mkdir exports
-cd exports
-mkdir certificates
-mkdir id-cards
-mkdir opito
-mkdir welcome-letter
-cd opito
-mkdir csv
-cd /data
-mkdir uploads
-cd uploads
-mkdir foets
-mkdir learner-ids
-mkdir pictures
-
 cd /data
 
 ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
